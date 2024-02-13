fb122e873ee8702e1f5868c3cf1421bb6cb841da
Sharanya Garipally <sharanya@Sharanyas-MacBook-Pro.local>
2024-02-12 23:51:52 -0500

    Added PersonalBudget to React

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Menu() {
+  return (
+    <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+    </footer>
+  );
+}
+
+export default Menu;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Menu() {
+  return (
+    <div class="hero">
+        <h1>Personal Budget</h1>
+        <h2 aria-level="3">A personal-budget management app</h2>
+    </div>
+  );
+}
+
+export default Menu;
